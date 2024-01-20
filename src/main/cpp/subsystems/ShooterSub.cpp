9ad6e6893e26a6c321963179cc65a22ce83d6d59
1828Boxerbots <1828boxerbots@gmail.com>
2024-01-20 13:24:10 -0700

    Skyler - Added Shooter Subsystem
    No code

---
+++ src/main/cpp/subsystems/ShooterSub.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/ShooterSub.h"
+
+ShooterSub::ShooterSub() = default;
+
+// This method will be called once per scheduler run
+void ShooterSub::Periodic() {}
